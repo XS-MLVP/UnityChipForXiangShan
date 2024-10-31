76bd7013928ed6b114653a035b37703d10ec0d05
yaozhicheng <yaozhicheng@ict.ac.cn>
2024-10-31 19:43:03 +0800

    use python download

--- Makefile
+++ Makefile
@@ -7,10 +7,7 @@ export CFG := ""
 RLT_DOWNLOAD_FROM="https://raw.githubusercontent.com/XS-MLVP/UnityChipXiangShanRTLs/refs/heads/main/README.md"
 RLT_DOWNLOAD_URL_BASE="https://github.com/XS-MLVP/UnityChipXiangShanRTLs/releases/download/"
 
-all: rtl dut_all test_all
-
-init:
-	echo "init this repo"
+all: dut_all test_all
 
 clean:
 	rm -rf out/*
@@ -34,33 +31,7 @@ dut_all:
     done
 
 rtl:
-	@RLT_DOWNLOAD_URL="";\
-	if [ -L rtl/rtl ] && [ -e rtl/rtl ]; then \
-		echo "RTL already exists. If you need update, please delete the rtl directories manually."; \
-	else \
-		if [ "$(target)" ]; then \
-			RLT_DOWNLOAD_URL="$(RLT_DOWNLOAD_URL_BASE)$(target)"; \
-		else \
-			echo "Search latest RTL from: $(RLT_DOWNLOAD_FROM)"; \
-			RLT_DOWNLOAD_URL=`curl $(RLT_DOWNLOAD_FROM)|grep -m 1 openxiangshan-kmh-|grep -oP '(?<=\().*?(?=\))'`; \
-	    fi;\
-		RTL_ZIP=$$(basename $$RLT_DOWNLOAD_URL);\
-		RTL_DIR=$${RTL_ZIP%.tar.gz};\
-		if [ -f rtl/$$RTL_ZIP ]; then \
-			echo "RTL tarball exists, ignore download"; \
-		else \
-			echo "Download: $$RLT_DOWNLOAD_URL"; \
-			wget -P rtl/ $$RLT_DOWNLOAD_URL; \
-		fi;\
-		if [ -d rtl/$$RTL_DIR ]; then \
-            echo "RTL directory already exists, ignore extract"; \
-        else \
-            echo "Extracting $$RTL_ZIP to rtl/$$RTL_DIR"; \
-            mkdir -p rtl/$$RTL_DIR; \
-            tar -xzf rtl/$$RTL_ZIP -C rtl/$$RTL_DIR --strip-components=1; \
-        fi;\
-		ln -s $$RTL_DIR rtl/rtl;\
-	fi;\
+	python3 run.py --config $(CFG)  --download-rtl
 
 doc:
 	cd documents && hugo server
--- comm/functions.py
+++ comm/functions.py
@@ -14,6 +14,11 @@
 import os
 import time
 import base64
+import re
+import requests
+from .logger import warning, debug
+from .cfg import get_config
+
 
 def merge_dict(dict1, dict2):
     """
@@ -33,21 +38,22 @@ def merge_dict(dict1, dict2):
             dict1[key] = dict2[key]
     return dict1
 
-def get_out_dir(subdir=""):
+
+def get_abs_path(path, sub, cfg):
+    if path.startswith("/"):
+        return os.path.join(path, sub)
     return os.path.abspath(os.path.join(
-        os.path.dirname(__file__), '../out', subdir))
+        os.path.dirname(cfg.__file__), path, sub))
 
 
-def debug(msg):
-    print(msg)
+def get_out_dir(subdir="", cfg=None):
+    cfg = get_config(cfg)
+    return get_abs_path(cfg.output.out_dir, subdir, cfg)
 
 
-def is_full_test():
-    return os.getenv("XS_FULL_TEST") == "1" or os.getenv("XS_FULL_TEST") == "true"
-
-
-def is_short_test():
-    return not is_full_test()
+def get_rtl_dir(subdir="", cfg=None):
+    cfg = get_config(cfg)
+    return get_abs_path(cfg.rtl.cache_dir, subdir, cfg)
 
 
 def time_format(seconds=None, fmt="%H-%M-%S"):
@@ -70,3 +76,73 @@ def base64_decode(base64_str):
     base64_bytes = base64_str.encode('utf-8')
     input_bytes = base64.b64decode(base64_bytes)
     return input_bytes.decode('utf-8')
+
+
+def use_rtl(rtl_file, out_dir):
+    rtl_path = os.path.join(out_dir, rtl_file)
+    dir_name = os.path.basename(rtl_file).replace(".tar.gz", "")
+    rtl_dir = os.path.join(out_dir, dir_name)
+    if not os.path.exists(rtl_dir):
+        debug("Extract %s to %s" % (rtl_path, out_dir))
+        assert os.system(f"tar -xvf {rtl_path} -C {out_dir}")==0, "Extract RTL failed"
+    lnk_file = os.path.join(out_dir, "rtl")
+    if os.path.exists(lnk_file):
+        debug("Remove old link %s" % lnk_file)
+        os.remove(lnk_file)
+    os.symlink(rtl_dir, lnk_file)
+
+
+def download_rtl(base_url, out_dir, version="latest"):
+    """
+    Download RTL from url
+    """
+    debug("Download RTL from %s (%s)", base_url, version)
+    if version != "latest":
+        for f in os.listdir(out_dir):
+            if version in f and "tar.gz" in f:
+                debug("find %s in %s, ignore download" % (f, out_dir))
+                use_rtl(f, out_dir)
+                return True
+        if version in base_url and ".tar.gz" in base_url:
+            os.system(f"wget {base_url} -P {out_dir}")
+        for f in os.listdir(out_dir):
+            if version in f and "tar.gz" in f:
+                debug("download %s success" % f)
+                use_rtl(f, out_dir)
+                return True
+    if not base_url.endswith(".tar.gz"):
+        resp = requests.get(base_url).content.decode('utf-8')
+        all_urls = {}
+        url = None
+        for u in re.findall(r'http[s]?://\S+?\.tar\.gz', resp):
+            all_urls[u.split("/")[-1].strip()] = u
+            if version and version in u:
+                url = u
+                break
+        if url is None:
+            if version:
+                warning(f"version {version} not found in {all_urls.keys()}, download the first one")
+            assert len(all_urls) > 0, "No download url found (resp: %s)" % resp
+            file_to_download = sorted(all_urls.keys())[0]
+            for f in os.listdir(out_dir):
+                if file_to_download in f and "tar.gz" in f:
+                    debug("find %s in %s, ignore download", f, out_dir)
+                    use_rtl(f, out_dir)
+                    return True
+            url = all_urls[file_to_download]
+        debug(f"download {url} to {out_dir}")
+        assert os.system(f"wget {url} -P {out_dir}") == 0, "Download RTL failed"
+        use_rtl(url.split("/")[-1], out_dir)
+    else:
+        assert os.system(f"wget {base_url} -P {out_dir}") == 0, "Download RTL failed"
+        use_rtl(base_url.split("/")[-1], out_dir)
+    return True
+
+
+def is_full_test():
+    cfg = get_config()
+    return cfg.test.test_model == "full_test"
+
+
+def is_short_test():
+    return not is_full_test()
