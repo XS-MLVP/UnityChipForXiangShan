cc01b5ddd1d04b21e79b1574584554b42c3a7d36
cyn1456492382 <sweatheart@LAPTOP-KS3EPQ8A.>
2024-12-03 15:14:57 +0800

    modify compile file

--- scripts/build_ut_mem_block_load_queue_rar.py
+++ scripts/build_ut_mem_block_load_queue_rar.py
@@ -19,21 +19,21 @@ from comm import warning, info
 def build(cfg):
     # import base modules
     from toffee_test.markers import match_version
-    from comm import is_all_file_exist, get_rtl_dir, exe_cmd, get_root_dir
+    from comm import is_all_file_exist, get_rtl_dir, exe_cmd, get_root_dir, get_out_dir
     # check version
     if not match_version(cfg.rtl.version, []):
         warning("backend_ctrlblock_decode: %s" % f"Unsupported RTL version {cfg.rtl.version}")
         return False
     # check files
-    f = is_all_file_exist(["rtl/LoadQueueRAR.sv", "rtl/LqPAddrModule.sv", "rtl/FreeList_3.sv", "DelayNWithValid_14.sv", "DelayN_196.sv", "DelayNWithValid_11.sv"], get_rtl_dir(cfg=cfg))
+    f = is_all_file_exist(["rtl/LoadQueueRAR.sv", "rtl/LqPAddrModule.sv", "rtl/FreeList_3.sv", "rtl/DelayNWithValid_14.sv", "rtl/DelayN_196.sv", "rtl/DelayNWithValid_11.sv"], get_rtl_dir(cfg=cfg))
     assert f is True, f"File {f} not found"
     # build
-    # export LoadQueueRAR.sv
+    # export LoadQueueRAR
     if not os.path.exists(get_root_dir("dut/LoadQueueRAR")):
         info("Exporting LoadQueueRAR.sv")
-        s, out, err = exe_cmd(f'picker export {get_rtl_dir("rtl/LoadQueueRAR.sv", cfg=cfg)} --lang python --fs {get_rtl_dir("lsq-test/LoadQueueRAR.txt", cfg=cfg)} --sname LoadQueueRAR --tdir {get_root_dir("dut")}/')
+        s, out, err = exe_cmd(f'picker export --cp_lib false {get_rtl_dir("rtl/LoadQueueRAR.sv", cfg=cfg)} --fs {get_root_dir("scripts/mem_block_load_queue_rar/rtl_files.f")} --lang python --tdir {get_root_dir("dut")}/ -c')
         assert s, "Failed to export LoadQueueRAR.sv: %s\n%s" % (out, err)
     return True
 
 def line_coverage_files(cfg):
-    return ["LoadQueueRAR.v", "LqPAddrModule.v", "FreeList_3.v"]
+    return ["LoadQueueRAR.v"]
---
+++ scripts/mem_block_load_queue_rar/rtl_files.f
@@ -0,0 +1,6 @@
+../../rtl/rtl/LqPAddrModule.sv
+../../rtl/rtl/FreeList_3.sv
+#
+../../rtl/rtl/DelayNWithValid_14.sv
+../../rtl/rtl/DelayN_196.sv
+../../rtl/rtl/DelayNWithValid_11.sv
