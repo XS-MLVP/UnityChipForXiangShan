a866912fc3a6df3d17b857a1a898f6f9e5c5957c
cjh <cjh@DESKTOP-7N99OEH.lan>
2024-12-18 14:14:46 +0800

    predecode: io description added in documents

--- ut_frontend/ifu/pred_checker/agent/__init__.py
+++ ut_frontend/ifu/pred_checker/agent/__init__.py
@@ -12,23 +12,17 @@ class PredCheckerAgent(Agent):
         self.bundle.io._in._ftqOffset._bits.value = ftqOffBits
         self.bundle.io._in._target.value = tgt
         self.bundle.io._in._fire_in.value = fire
-        # print(self.bundle.io._in.ftqOffset_valid.value)
         print("binds_single_finished")
-        # print(len(self.bundle.io._in.pc))
         for i in range(PREDICT_WIDTH):
             getattr(self.bundle.io._in._pc, f'_{i}').value = pc[i]
             getattr(self.bundle.io._in._instrRange, f'_{i}').value = instrRange[i]
             getattr(self.bundle.io._in._instrValid, f'_{i}').value = instrValid[i]
             getattr(self.bundle.io._in._jumpOffset, f'_{i}').value = jumpOffset[i]
             
-            # pd = getattr(self.bundle.io._in, f'pds_{i}_')
-            # pd = self.bundle.io._in.pds[i]
             getattr(self.bundle.io._in._pds, f'_{i}')._isRVC.value = pds[i][RVC_LABEL]
             getattr(self.bundle.io._in._pds, f'_{i}')._brType.value = pds[i][BRTYPE_LABEL]
             getattr(self.bundle.io._in._pds, f'_{i}')._isRet.value = pds[i][RET_LABEL]
-            # print(pd.brType.value)
         await self.bundle.step()
-        # await self.bundle.step()
         stg1_fixedRange = [getattr(self.bundle.io._out._stage1Out._fixedRange, f'_{i}').value for i in range(PREDICT_WIDTH)]
         stg1_fixedTaken = [getattr(self.bundle.io._out._stage1Out._fixedTaken, f'_{i}').value for i in range(PREDICT_WIDTH)]
         yield stg1_fixedRange, stg1_fixedTaken
