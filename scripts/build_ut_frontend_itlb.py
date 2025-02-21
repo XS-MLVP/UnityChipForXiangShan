1bcaa98e96a9bc8404c5e71c8490aca66f56722b
CharlesJunic <130619373+CharlesJunic@users.noreply.github.com>
2025-02-21 16:44:00 +0800

    add: itlb env, update /document/zh_cn/docs/verify_env.md & /document/README.md (#74)

--- scripts/build_ut_frontend_itlb.py
+++ scripts/build_ut_frontend_itlb.py
@@ -14,11 +14,33 @@
 
 
 import os
+from comm import warning, info
 
 
 def build(cfg):
-    return False
+    # import base modules
+    from toffee_test.markers import match_version
+    from comm import is_all_file_exist, get_rtl_dir, exe_cmd, get_root_dir
+    # check version
+    if not match_version(cfg.rtl.version, "openxiangshan-kmh-*"):
+        warning("frontend_itlb: %s" % f"Unsupported RTL version {cfg.rtl.version}")
+        return False
+    # check files
+    f = is_all_file_exist(["rtl/TLB.sv"], get_rtl_dir(cfg=cfg))
+    assert f is True, f"File {f} not found"
+    # build
+    # export TLB.sv
+    if not os.path.exists(get_root_dir("dut/TLB")):
+        info("Exporting TLB.sv")
+        s, _, _ = exe_cmd(f'picker export --cp_lib false {get_rtl_dir("rtl/TLB.sv", cfg=cfg)} --fs {get_root_dir("scripts/frontend_itlb/rtl_files.f")} --lang python --tdir {get_root_dir("dut")}/ -w itlb.fst -c')
+        assert s, "Failed to export TLB.sv"
+    # build disasm
+    if not os.path.exists(get_root_dir("tools/disasm/build")):
+        info("Building disasm")
+        s, _, _ = exe_cmd("make -C %s" % get_root_dir("tools/disasm"))
+        assert s, "Failed to build disasm"
+    return True
 
 
 def line_coverage_files(cfg):
-    return []
+    return ["TLB.v"]
