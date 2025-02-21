6a30e36e2462727e36ba3c246536663d12126369
cjh <211250069@smail.nju.edu.cn>
2025-02-21 19:50:00 +0800

    remove tagesc infos

--- scripts/build_ut_frontend_ifu_top.py
+++ scripts/build_ut_frontend_ifu_top.py
@@ -7,9 +7,9 @@ def build(cfg):
     from comm import error, info, get_root_dir, exe_cmd
     # check version
     if not match_version(cfg.rtl.version, "openxiangshan-kmh-*"):
-        error(f"frontend_bpu_tagesc: Unsupported RTL version {cfg.rtl.version}")
+        error(f"frontend_ifu_frontendTrigger: Unsupported RTL version {cfg.rtl.version}")
         return False
-    # find source files for Tage_SC
+    # find source files for FrontendTrigger
     rtl_files = get_all_rtl_files("NewIFU", cfg=cfg)
     info(f"rtl_files: {rtl_files}")
     assert rtl_files, "Cannot find RTL files of Frontend.IFU"
@@ -17,7 +17,7 @@ def build(cfg):
     internal_signals_path = os.path.join(get_root_dir("scripts/ifu_related/ifu_top_internals.yaml"))
     # assert os.path.exists(internal_signals_path), "Cannot find internal signal files"
 
-    # export Tage_SC.sv
+    # export FrontendTrigger.sv
     if not os.path.exists(get_root_dir("dut/IFU")):
         info("Exporting IFU.sv")
         with NamedTemporaryFile("w+", encoding="utf-8", suffix=".txt") as filelist:
@@ -25,7 +25,7 @@ def build(cfg):
             filelist.flush()
             s, _, err = exe_cmd(
                 f"picker export --cp_lib false {rtl_files[0]} --fs {filelist.name} --lang python --tdir " 
-                f"{get_root_dir('dut')}/ -w Tage_SC.fst -c --internal={internal_signals_path}")
+                f"{get_root_dir('dut')}/ -w FrontendTrigger.fst -c --internal={internal_signals_path}")
         assert s, err
     return True
 
