ffb9c98b0860a824f5b314b2179a3bdf0911b784
cjh <211250069@smail.nju.edu.cn>
2025-02-28 11:23:58 +0800

    add ifu_top env initially

---
+++ ut_frontend/ifu/ifu_top/datadef/__init__.py
@@ -0,0 +1,5 @@
+from .ftq_datadef import FTQQuery, FTQFlushInfo, FTQResp, FTQIdx
+from .icache_datadef import ICacheStatusResp
+from .frontend_trigger_datadef import FrontendTriggerReq
+from .ibuffer_datadef import ToIbufferAllRes
+from .mmio_related import ITLBReq, ITLBResp, ToUncache, FromUncache, PMPResp, RobCommit
