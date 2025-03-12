d452d7af8a21784adee8ec0723169c6a57044f36
cyn1456492382 <sweatheart@LAPTOP-KS3EPQ8A.>
2025-03-12 10:28:20 +0800

    add checkpoints

--- ut_mem_block/load_queue/rar/test/test_spec_case.py
+++ ut_mem_block/load_queue/rar/test/test_spec_case.py
@@ -4,6 +4,7 @@ import toffee_test
 import toffee
 from comm.functions import get_out_dir, get_root_dir
 from dut.LoadQueueRAR import DUTLoadQueueRAR
+from .checkpoints_update_register import get_coverage_group_of_update_register
 from .checkpoints_rar_static import init_rar_funcov
 from ..util.dataclass import IOQuery, IOldWbPtr, IORedirect, IORelease
 from ..env.LoadQueueRAREnv import LoadQueueRAREnv
@@ -377,6 +378,15 @@ async def test_freelist_full(loadqueue_rar_env: LoadQueueRAREnv):
         allocated.append(getattr(loadqueue_rar_env.agent.bundle.LoadQueueRAR._allocated, f'_{i}').value)
     allocate = allocated.count(1)
     assert allocate == 72
+    
+    redirect = IORedirect(valid=True, robIdx_flag=True, robIdx_value=56, level=0)
+    ldWbPtr = IOldWbPtr(flag=True, value=58)
+    inner = await loadqueue_rar_env.agent.Dequeue(ldWbPtr, redirect)
+    allocated = []
+    for i in range(72):
+        allocated.append(getattr(inner._allocated, f'_{i}').value)
+    allocate = allocated.count(1)
+    assert allocate == 0
   
     await loadqueue_rar_env.agent.reset()
     for i in range(24):
@@ -420,6 +430,39 @@ async def test_freelist_full(loadqueue_rar_env: LoadQueueRAREnv):
     allocate = allocated.count(1)
     assert allocate == 70
     
+@toffee_test.testcase
+async def test_random(loadqueue_rar_env: LoadQueueRAREnv):
+    random.seed(os.urandom(128))
+    await loadqueue_rar_env.agent.reset()
+    num_queries = 3
+    for _ in range(5000):
+        query = [
+            IOQuery(
+                req_valid=random.choice([True, False]),
+                uop_robIdx_flag=random.choice([True, False]),
+                uop_robIdx_value=random.randint(0, 100),
+                uop_lqIdx_flag=random.choice([True, False]),
+                uop_lqIdx_value=random.randint(0, 71),
+                bits_paddr=generate_random_48bit_integer(),
+                data_valid=random.choice([True, False]),
+                is_nc=random.choice([True, False]),
+                revoke=random.choice([True, False])
+            )
+            for i in range(num_queries)
+        ]
+        redirect = IORedirect(
+            valid=random.choice([True, False]),
+            robIdx_flag=random.choice([True, False]),
+            robIdx_value=random.randint(0, 100),
+            level=random.randint(0, 1)
+        )
+        ldWbPtr = IOldWbPtr(
+            flag=random.choice([True, False]),
+            value=random.randint(0, 10)  # 假设 value 的范围是 0 到 10
+        )
+        _ = await loadqueue_rar_env.agent.Enqueue(query, redirect, ldWbPtr)
+
+    
 @toffee_test.fixture
 async def loadqueue_rar_env(toffee_request: toffee_test.ToffeeRequest):
     import asyncio
@@ -427,6 +470,9 @@ async def loadqueue_rar_env(toffee_request: toffee_test.ToffeeRequest):
     toffee.start_clock(dut)
     env = LoadQueueRAREnv(dut)
     toffee_request.add_cov_groups(init_rar_funcov(env))
+    toffee_request.add_cov_groups([
+        get_coverage_group_of_update_register(env)
+    ])
     
     yield env
     
@@ -438,3 +484,4 @@ async def loadqueue_rar_env(toffee_request: toffee_test.ToffeeRequest):
                 await task
             except asyncio.CancelledError:
                 break
+            
