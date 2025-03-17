7db88beffdba01de50661df2809f74c1977d7342
CharlesJunic <bidingjun21@mails.ucas.ac.cn>
2025-03-17 18:03:11 +0800

    update itlb env

--- scripts/build_ut_frontend_tlb_storage_wrapper.py
+++ scripts/build_ut_frontend_tlb_storage_wrapper.py
@@ -14,31 +14,37 @@
 
 
 import os
-from comm import warning, info
+from comm import warning, info, get_all_rtl_files
 
 
 def build(cfg):
     # import base modules
+    from tempfile import NamedTemporaryFile
     from toffee_test.markers import match_version
     from comm import is_all_file_exist, get_rtl_dir, exe_cmd, get_root_dir
     # check version
     if not match_version(cfg.rtl.version, "openxiangshan-kmh-*"):
-        warning("frontend_itlb: %s" % f"Unsupported RTL version {cfg.rtl.version}")
+        warning("frontend_tlb_storage_wrapper: %s" % f"Unsupported RTL version {cfg.rtl.version}")
         return False
     # check files
     f = is_all_file_exist(["rtl/TlbStorageWrapper.sv"], get_rtl_dir(cfg=cfg))
     assert f is True, f"File {f} not found"
+    # find source files for TlbStorageWrapper
+    rtl_files = get_all_rtl_files("TlbStorageWrapper", cfg=cfg)
+    info(f"rtl_files: {rtl_files}")
+    assert rtl_files, "Cannot find RTL files of TlbStorageWrapper"
+
     # build
     # export TlbStorageWrapper.sv
     if not os.path.exists(get_root_dir("dut/TlbStorageWrapper")):
         info("Exporting TlbStorageWrapper.sv")
-        s, _, _ = exe_cmd(f'picker export --cp_lib false {get_rtl_dir("rtl/TlbStorageWrapper.sv", cfg=cfg)} --fs {get_root_dir("scripts/frontend_itlb/tlb_storage_wrapper_rtl_files.f")} --lang python --tdir {get_root_dir("dut")}/ -w TlbStorageWrapper.fst -c')
-        assert s, "Failed to export TlbStorageWrapper.sv"
-    # build disasm
-    if not os.path.exists(get_root_dir("tools/disasm/build")):
-        info("Building disasm")
-        s, _, _ = exe_cmd("make -C %s" % get_root_dir("tools/disasm"))
-        assert s, "Failed to build disasm"
+        with NamedTemporaryFile("w+", encoding="utf-8", suffix=".txt") as filelist:
+            filelist.write("\n".join(rtl_files))
+            filelist.flush()
+            s, _, err = exe_cmd(
+                f"picker export --cp_lib false {rtl_files[0]} --fs {filelist.name} --lang python --tdir " 
+                f"{get_root_dir('dut')}/ -w TlbStorageWrapper.fst -c")
+        assert s, err
     return True
 
 
--- ut_frontend/itlb/classical_version/test_tlb_receive_request.py
+++ ut_frontend/itlb/classical_version/test_tlb_receive_request.py
@@ -13,36 +13,122 @@
 #**************************************************************************************/
 
 from .env import *
+import inspect
 
-# test example
-def test_req_from_icache_min_vaddr(tlb_fixture):
+### CASE EXAMPLE
+# Running the following test case will show a pass:
+#     In this test case, a single port (Requestor0) sends an address translation 
+# request to TLB. TLB is expected to return a miss in the next cycle and initiate 
+# a request to the PTW. At this point, you can simultaneously verify whether the 
+# vpn in the PTW request is correct.
+def test_req_from_icache_rand_vaddr_rand_valid_single_port(tlb_fixture):
+    """
+    Compare the PTW request with the reference:
+        rand vaddr
+        rand valid
+        single port 0 / 1
+    """
     # connect to fixture
     tlb = tlb_fixture
-    # reset
-    tlb.reset()
+    # add watch point
+    case_name = inspect.currentframe().f_back.f_code.co_name
+    g.add_watch_point(tlb.requestor_0.resp.miss, {
+                        "miss": fc.Eq(1),
+                        "hit": fc.Eq(0),
+    }, name = f"{case_name}: REQUESTOR_0_MISS")
+    g.add_watch_point(tlb.requestor_1.resp.miss, {
+                        "miss": fc.Eq(1),
+                        "hit": fc.Eq(0),
+    }, name = f"{case_name}: REQUESTOR_1_MISS")
+    g.add_watch_point(tlb.ctrl.io_ptw_req_0_valid, {
+                        "valid": fc.Eq(1),
+                        "invalid": fc.Eq(0),
+    }, name = f"{case_name}: PTW_REQ_0_VALID")
+    g.add_watch_point(tlb.ctrl.io_ptw_req_1_valid, {
+                        "valid": fc.Eq(1),
+                        "invalid": fc.Eq(0),
+    }, name = f"{case_name}: PTW_REQ_1_VALID")
     # set default value
     tlb.set_default_value()
-    # add function point
-    g.add_watch_point(tlb.ptw_req_0.vpn, 
-                          {"ptw_req_0_vpn_1": toffee.funcov.Eq(0)}, name = "ptw_req_0_vpn_1")
-    g.add_watch_point(tlb.ptw_req_1.vpn, 
-                          {"ptw_req_1_vpn_1": toffee.funcov.Eq(0)}, name = "ptw_req_1_vpn_1")
+    # reset
+    tlb.reset()
 
     # add clock
     tlb.dut.xclock.StepRis(lambda _: g.sample())
     # start
-    for _ in range(1000):
-        # add signal
-        req_0_valid = random.choice([0, 1])
-        req_1_valid = 1 - req_0_valid
-        req_0_vaddr = 0
-        req_1_vaddr = 0
+    for _ in range(10000):
+        # add signal and assign to dut
+        req_0_valid, req_0_vaddr = tlb.rand_req0()
+        # step to next cycle
+        tlb.dut.Step(2)
+        
+        # assert result
+        assert (tlb.ctrl.io_ptw_req_0_valid.value == req_0_valid)
+        if (req_0_valid):
+            assert (tlb.ptw_req_0.vpn.value == req_0_vaddr >> 12)
+    #################################### TODO EXAMPLE ####################################
+    # TODO: Add Requestor1 Verification
+    #     The existing test case provides verification methods for sending TLB requests 
+    # from requestor0. Now, we need to verify whether sending TLB requests from requestor1 
+    # also meets the expected behavior. Modify the test case such that in each cycle, a 
+    # TLB request is sent from either requestor0 or requestor1, with strict attention to 
+    # ensuring that both ports are not active simultaneously.
+    for _ in range(10000):
+        # add signal and assign to dut
+        req_1_valid, req_1_vaddr = tlb.rand_req1()
+        # step to next cycle
+        tlb.dut.Step(2)
+        
+        # assert result
+        assert (tlb.ctrl.io_ptw_req_1_valid.value == req_1_valid)
+        if (req_1_valid):
+            assert (tlb.ptw_req_1.vpn.value == req_1_vaddr >> 12)
+    ######################################################################################
+    ######################################################################################
+    # TODO: Add Requestor2 Verification (Critical Differences)
+    #     Requestor2 uses BLOCKING access protocol (vs. Non-blocking on 0/1)
+    ######################################################################################
 
-        # assign to dut
-        tlb.requestor_0.req.valid.value = req_0_valid
-        tlb.requestor_1.req.valid.value = req_1_valid
-        tlb.requestor_0.req.bits_vaddr.value = req_0_vaddr
-        tlb.requestor_1.req.bits_vaddr.value = req_1_vaddr
+### CASE EXAMPLE
+def test_req_from_icache_rand_vaddr_rand_valid_muti_port(tlb_fixture):
+    """
+    Func: compare the PTW request with the reference:
+        subfunc1: rand vaddr
+        subfunc2: rand valid
+        subfunc3: muti port 0 & 1
+    """
+    # connect to fixture
+    tlb = tlb_fixture
+    # add watch point
+    case_name = inspect.currentframe().f_back.f_code.co_name
+    g.add_watch_point(tlb.requestor_0.resp.miss, {
+                        "miss": fc.Eq(1),
+                        "hit": fc.Eq(0),
+    }, name = f"{case_name}: REQUESTOR_0_MISS")
+    g.add_watch_point(tlb.requestor_1.resp.miss, {
+                        "miss": fc.Eq(1),
+                        "hit": fc.Eq(0),
+    }, name = f"{case_name}: REQUESTOR_1_MISS")
+    g.add_watch_point(tlb.ctrl.io_ptw_req_0_valid, {
+                        "valid": fc.Eq(1),
+                        "invalid": fc.Eq(0),
+    }, name = f"{case_name}: PTW_REQ_0_VALID")
+    g.add_watch_point(tlb.ctrl.io_ptw_req_1_valid, {
+                        "valid": fc.Eq(1),
+                        "invalid": fc.Eq(0),
+    }, name = f"{case_name}: PTW_REQ_1_VALID")
+    # set default value
+    tlb.set_default_value()
+    # reset
+    tlb.reset()
+
+    # add clock
+    tlb.dut.xclock.StepRis(lambda _: g.sample())
+    # start
+    for _ in range(10000):
+        # add signal and assign to dut
+        req_0_valid, req_0_vaddr = tlb.rand_req0()
+        req_1_valid, req_1_vaddr = tlb.rand_req1()
         # step to next cycle
         tlb.dut.Step(2)
         
