a30b9eae7cc4f371a3fed90ae64fe3f40d41dc9c
Knight of Thunder <142707302+Knight-of-Thunder@users.noreply.github.com>
2025-06-07 18:18:00 +0800

    add -t option to the picker command in ftq_top script to allow repeated directory creation (#137)

--- scripts/build_ut_frontend_ftq_pc_mem.py
+++ scripts/build_ut_frontend_ftq_pc_mem.py
@@ -4,7 +4,7 @@ from comm import warning, info
 def build(cfg):
     # import base modules
     from toffee_test.markers import match_version
-    from comm import is_all_file_exist, get_rtl_dir, exe_cmd, get_root_dir
+    from comm import is_all_file_exist, get_rtl_dir, exe_cmd, get_root_dir, get_all_rtl_files
     # check version
     if not match_version(cfg.rtl.version, "openxiangshan-kmh-*"):
         warning("frontend_ftq_redirect_mem: %s" % f"Unsupported RTL version {cfg.rtl.version}")
@@ -12,24 +12,15 @@ def build(cfg):
     # check files
     module_name = "FtqPcMem"
     file_name ="FtqPcMemWrapper.sv"
-    dp_file_names = [
-    "SyncDataModuleTemplate_FtqPC_64entry.sv", 
-    "DataModule_FtqPC_16entry.sv"
-    ]
-    dp_fpaths = [f"rtl/rtl/{dp_file_name}" for dp_file_name in dp_file_names]
-    dp_fpaths_after_get_root = [get_root_dir(dp_fpath) for dp_fpath in dp_fpaths]
-    fpath = f"rtl/{file_name}"
-    all_fpaths = dp_fpaths + [fpath]
-    ## internal signals is now not determined
+    rtl_files = get_all_rtl_files("FtqPcMemWrapper", cfg=cfg)
     internal_signals_path=""
-    f = is_all_file_exist(all_fpaths, get_rtl_dir(cfg=cfg))
-    #assert f is True, f"File {f} not found"
+
     # build
     # export SyncDataModuleTemplate__64entry.sv
     if not os.path.exists(get_root_dir(f"dut/{module_name}")):
         info(f"Exporting {file_name}.sv")
-        s,out,err = exe_cmd(f'picker export {get_rtl_dir(f"{fpath}",cfg = cfg)} --tname {module_name}\
-                            --lang python --tdir {get_root_dir("dut")}/ -w {module_name}.fst -c --fs ' + ' '.join(dp_fpaths_after_get_root))
+        s,out,err = exe_cmd(f'picker export {rtl_files[0]} --tname {module_name}\
+                            --lang python --tdir {get_root_dir("dut")}/ -w {module_name}.fst -c --fs ' + ' '.join(rtl_files))
         assert s, f"Failed to export {file_name}.sv: %s\n%s" % (out, err)
 
     return True
