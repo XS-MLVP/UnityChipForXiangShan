d4cb1b19078bbb86146a1e8b986a478fe3de3f51
mingyu shi <mingyu.shi@smail.nju.edu.cn>
2025-08-07 15:35:41 +0800

    fix bugs of lambda

--- ut_frontend/ifu/predecode/test/predecode_fixture.py
+++ ut_frontend/ifu/predecode/test/predecode_fixture.py
@@ -9,35 +9,63 @@ grp = fc.CovGroup(UT_FCOV("../../CLASSIC"))
 
 def init_cov(dut:DUTPreDecode, grp: fc.CovGroup):
     for i in range(16):
-        grp.add_cover_point(dut,{
-            "instr is rvc:": lambda x: getattr(dut,f'io_out_pd_{i}_isRVC').value == 1,
-            "instr is rvi:": lambda x: getattr(dut,f'io_out_pd_{i}_isRVC').value == 0,
-        },name=f'check_rvc_rvi_{i}')
-    
+        grp.add_cover_point(
+            dut,
+            {
+                "instr is rvc:": lambda x, current_i=i: getattr(
+                    x, f"io_out_pd_{current_i}_isRVC"
+                ).value
+                == 1,
+                "instr is rvi:": lambda x, current_i=i: getattr(
+                    x, f"io_out_pd_{current_i}_isRVC"
+                ).value
+                == 0,
+            },
+            name=f"check_rvc_rvi_{i}",
+        )
+
     for i in range(2,16):
-        grp.add_cover_point(dut,{
-            "instr is half_valid_start:": lambda x: getattr(dut,f'io_out_hasHalfValid_{i}').value == 1,
-            "instr is not half_valid_start:": lambda x: getattr(dut,f'io_out_hasHalfValid_{i}').value == 0,
-        },name=f'check_half_valid_start_{i}')
-    
+        grp.add_cover_point(
+            dut,
+            {
+                "instr is half_valid_start:": lambda x, current_i=i: getattr(
+                    x, f"io_out_hasHalfValid_{current_i}"
+                ).value
+                == 1,
+                "instr is not half_valid_start:": lambda x, current_i=i: getattr(
+                    x, f"io_out_hasHalfValid_{current_i}"
+                ).value
+                == 0,
+            },
+            name=f"check_half_valid_start_{i}",
+        )
+
     for i in range(1,16):
-        grp.add_cover_point(dut,{
-            "instr is valid_starts:": lambda x: getattr(dut,f'io_out_pd_{i}_valid').value == 1,
-            "instr is not valid_starts:": lambda x: getattr(dut,f'io_out_pd_{i}_valid').value == 0,
-        },name=f'check_valid_start_{i}')
-
-
+        grp.add_cover_point(
+            dut,
+            {
+                "instr is valid_starts:": lambda x, current_i=i: getattr(
+                    x, f"io_out_pd_{current_i}_valid"
+                ).value
+                == 1,
+                "instr is not valid_starts:": lambda x, current_i=i: getattr(
+                    x, f"io_out_pd_{current_i}_valid"
+                ).value
+                == 0,
+            },
+            name=f"check_valid_start_{i}",
+        )
 
     def _mark(name):
         return module_name_with(name, "../predecode_test")
-    
+
     for i in range(16):
         grp.mark_function(f'check_rvc_rvi_{i}',_mark(["test_rvc_rvi_checker_2_1_2","test_rvc_rvi_checker_2_1_1"]))
     for i in range(2,16):
         grp.mark_function(f'check_half_valid_start_{i}',_mark(["test_precoding_checker_2_3_2"]))
     for i in range(1,16):
         grp.mark_function(f'check_valid_start_{i}',_mark(["test_precoding_checker_2_3_1"]))
-        
+
     return grp
 
 
