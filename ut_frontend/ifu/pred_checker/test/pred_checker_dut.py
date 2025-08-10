5520555d5ca79236d3a5543a0429bb1518262f8b
Shio2077 <darlingshio@mail.com>
2025-08-10 22:06:26 +0800

    Fixed errors in coverage collection

--- ut_frontend/ifu/pred_checker/test/pred_checker_dut.py
+++ ut_frontend/ifu/pred_checker/test/pred_checker_dut.py
@@ -19,10 +19,10 @@ def init_pred_checker_funcov(dut:DUTPredChecker, g:fc.CovGroup, env:PredCheckerE
         g.add_watch_point(dut, {
             "JAL_PRED_VALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 1,
             "JAL_PRED_INVALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 0,
-            f"JAL_INSTR_VALID_{j}": lambda dut: getattr(dut, f"io_in_instrValid_{j}").value == 1,
-            f"JAL_INSTR_RANGE_{j}": lambda dut: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j,
-            f"JAL_PRED_OFFSET_AT_{j}": lambda dut: getattr(dut, f"io_in_ftqOffset_bits").value == j,
-            f"JAL_PDS_INFO_AT_{j}": lambda dut: getattr(dut, f"io_in_pds_{j}_brType").value == 2
+            f"JAL_INSTR_VALID_{j}": lambda dut, j=j: getattr(dut, f"io_in_instrValid_{j}").value == 1,
+            f"JAL_INSTR_RANGE_{j+1}": lambda dut, j=j: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j+1,
+            f"JAL_PRED_OFFSET_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_ftqOffset_bits").value == j,
+            f"JAL_PDS_INFO_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_pds_{j}_brType").value == 2
         }, name=f"JAL_PRED_COV_{j}")
     
     # For function point 2 - RET prediction error checking:
@@ -30,10 +30,10 @@ def init_pred_checker_funcov(dut:DUTPredChecker, g:fc.CovGroup, env:PredCheckerE
         g.add_watch_point(dut, {
             "RET_PRED_VALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 1,
             "RET_PRED_INVALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 0,
-            f"RET_INSTR_VALID_{j}": lambda dut: getattr(dut, f"io_in_instrValid_{j}").value == 1,
-            f"RET_INSTR_RANGE_{j}": lambda dut: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j,
-            f"RET_PRED_OFFSET_AT_{j}": lambda dut: getattr(dut, f"io_in_ftqOffset_bits").value == j,
-            f"RET_PDS_INFO_AT_{j}": lambda dut: getattr(dut, f"io_in_pds_{j}_brType").value == 3
+            f"RET_INSTR_VALID_{j}": lambda dut, j=j: getattr(dut, f"io_in_instrValid_{j}").value == 1,
+            f"RET_INSTR_RANGE_{j+1}": lambda dut, j=j: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j+1,
+            f"RET_PRED_OFFSET_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_ftqOffset_bits").value == j,
+            f"RET_PDS_INFO_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_pds_{j}_brType").value == 3
         }, name=f"RET_PRED_COV_{j}")
     
     # For function point 3 - JALR prediction error checking:
@@ -41,16 +41,16 @@ def init_pred_checker_funcov(dut:DUTPredChecker, g:fc.CovGroup, env:PredCheckerE
         g.add_watch_point(dut, {
             "JALR_PRED_VALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 1,
             "JALR_PRED_INVALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 0,
-            f"JALR_INSTR_VALID_{j}": lambda dut: getattr(dut, f"io_in_instrValid_{j}").value == 1,
-            f"JALR_INSTR_RANGE_{j}": lambda dut: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j,
-            f"JALR_PRED_OFFSET_AT_{j}": lambda dut: getattr(dut, f"io_in_ftqOffset_bits").value == j,
-            f"JALR_PDS_INFO_AT_{j}": lambda dut: getattr(dut, f"io_in_pds_{j}_brType").value == 4
+            f"JALR_INSTR_VALID_{j}": lambda dut, j=j: getattr(dut, f"io_in_instrValid_{j}").value == 1,
+            f"JALR_INSTR_RANGE_{j+1}": lambda dut, j=j: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j+1,
+            f"JALR_PRED_OFFSET_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_ftqOffset_bits").value == j,
+            f"JALR_PDS_INFO_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_pds_{j}_brType").value == 3
         }, name=f"JALR_PRED_COV_{j}")
         
     # For function point 4 - Renewing instruction range:
     for j in range(PREDICT_WIDTH):
         g.add_watch_point(dut, {
-            f"RANGE_LENGTH_{j}_COV": lambda dut: sum(1 for i in range(PREDICT_WIDTH) if getattr(dut, f"io_in_instrRange_{i}").value == 1) == j
+            f"RANGE_LENGTH_{j+1}_COV": lambda dut, j=j: sum(1 for i in range(PREDICT_WIDTH) if getattr(dut, f"io_in_instrRange_{i}").value == 1) == j+1
         }, name=f"RANGE_FIXING_COV_{j}")
     
     # For function point 5 - Not-CFI instruction checking
@@ -58,10 +58,10 @@ def init_pred_checker_funcov(dut:DUTPredChecker, g:fc.CovGroup, env:PredCheckerE
         g.add_watch_point(dut,{
             "CFI_PRED_VALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 1,
             "CFI_PRED_INVALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 0,
-            f"CFI_INSTR_VALID_{j}": lambda dut: getattr(dut, f"io_in_instrValid_{j}").value == 1,
-            f"CFI_INSTR_RANGE_{j}": lambda dut: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j,
-            f"CFI_PRED_OFFSET_AT_{j}": lambda dut: getattr(dut, f"io_in_ftqOffset_bits").value == j,
-            f"CFI_PDS_INFO_AT_{j}": lambda dut: getattr(dut, f"io_in_pds_{j}_brType").value  > 0
+            f"CFI_INSTR_VALID_{j}": lambda dut, j=j: getattr(dut, f"io_in_instrValid_{j}").value == 1,
+            f"CFI_INSTR_RANGE_{j+1}": lambda dut, j=j: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j+1,
+            f"CFI_PRED_OFFSET_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_ftqOffset_bits").value == j,
+            f"CFI_PDS_INFO_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_pds_{j}_brType").value  > 0
         }, name=f"CFI_PRED_COV_{j}")
     
     # For function point 6 - Invalid instruction checking
@@ -69,9 +69,9 @@ def init_pred_checker_funcov(dut:DUTPredChecker, g:fc.CovGroup, env:PredCheckerE
         g.add_watch_point(dut,{
             "INV_PRED_VALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 1,
             "INV_PRED_INVALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 0,
-            f"INV_INSTR_OFFSET_{j}": lambda dut: getattr(dut, f"io_in_instrValid_{j}").value == 0,
-            f"INV_INSTR_RANGE_{j}": lambda dut: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j,
-            f"INV_PRED_OFFSET_AT_{j}": lambda dut: getattr(dut, f"io_in_ftqOffset_bits").value == j,
+            f"INV_INSTR_OFFSET_{j}": lambda dut, j=j: getattr(dut, f"io_in_instrValid_{j}").value == 0,
+            f"INV_INSTR_RANGE_{j+1}": lambda dut, j=j: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j+1,
+            f"INV_PRED_OFFSET_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_ftqOffset_bits").value == j,
         }, name=f"INV_PRED_COV_{j}")
     
     # For function point 7 - Target error checking
@@ -81,12 +81,11 @@ def init_pred_checker_funcov(dut:DUTPredChecker, g:fc.CovGroup, env:PredCheckerE
             "TGT_CFI_PRED_VALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 1,
             "TGT_CFI_PRED_INVALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 0,
             "TGT_CFI_INSTR_VALID": lambda dut: getattr(dut, f"io_in_instrValid_{j}").value == 1,
-            f"TGT_CFI_INSTR_RANGE_{j}": lambda dut: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j,
-            f"TGT_CFI_PRED_OFFSET_AT_{j}": lambda dut: getattr(dut, f"io_in_ftqOffset_bits").value == j,
-            f"TGT_CFI_PDS_INFO_AT_{j}": lambda dut: getattr(dut, f"io_in_pds_{j}_brType").value  > 0,
-            f"TGT_CFI_JMPOFFSET_VAL_{j}": lambda dut: getattr(dut, f"io_in_jumpOffset_{j}").value > 0,
+            f"TGT_CFI_PRED_OFFSET_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_ftqOffset_bits").value == j,
+            f"TGT_CFI_PDS_INFO_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_pds_{j}_brType").value  > 0,
+            f"TGT_CFI_JMPOFFSET_VAL_{j}": lambda dut, j=j: getattr(dut, f"io_in_jumpOffset_{j}").value > 0,
             f"TGT_CFI_TGT_VAL": lambda dut: getattr(dut, "io_in_target").value > 0,
-            f"TGT_CFI_PC_VAL_{j}": lambda dut: getattr(dut, f"io_in_pc_{j}").value > 2**50 - 2**6 - 4
+            f"TGT_CFI_PC_VAL_{j}": lambda dut, j=j: getattr(dut, f"io_in_pc_{j}").value > 2**50 - 2**6 - 4
             },
             name=f"TGT_ERROR_COV_{j}",
         )
@@ -96,21 +95,13 @@ def init_pred_checker_funcov(dut:DUTPredChecker, g:fc.CovGroup, env:PredCheckerE
         g.add_watch_point(dut,{
             "RAND_PRED_VALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 1,
             "RAND_PRED_INVALID": lambda dut: getattr(dut, "io_in_ftqOffset_valid").value == 0,
-            "RAND_INSTR_VALID": lambda dut: getattr(dut, f"io_in_instrValid_{j}").value == 1,
-            "RAND_INSTR_RANGE": lambda dut: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j,
-            f"RAND_PRED_OFFSET_AT_{j}": lambda dut: getattr(dut, f"io_in_ftqOffset_bits").value == j,
-            f"RAND_PDS_INFO_AT_{j}": lambda dut: getattr(dut, f"io_in_pds_{j}_brType").value  > 0,
-            f"RAND_JMPOFFSET_VAL_{j}": lambda dut: getattr(dut, f"io_in_jumpOffset_{j}").value > 0,
+            "RAND_INSTR_VALID": lambda dut, j=j: getattr(dut, f"io_in_instrValid_{j}").value == 1,
+            "RAND_INSTR_RANGE": lambda dut, j=j: sum(getattr(dut, f"io_in_instrRange_{i}").value for i in range(PREDICT_WIDTH)) == j+1,
+            f"RAND_PRED_OFFSET_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_ftqOffset_bits").value == j,
+            f"RAND_PDS_INFO_AT_{j}": lambda dut, j=j: getattr(dut, f"io_in_pds_{j}_brType").value  > 0,
+            f"RAND_JMPOFFSET_VAL_{j}": lambda dut, j=j: getattr(dut, f"io_in_jumpOffset_{j}").value > 0,
             f"RAND_TGT_VAL": lambda dut: getattr(dut, "io_in_target").value > 0,
-            f"RAND_PC_VAL_{j}": lambda dut: getattr(dut, f"io_in_pc_{j}").value > 2**50 - 2**6 - 4
-            #f"FIXED_TARGET_{i}_CORRECT": lambda dut: getattr(dut, f"io_out_stage2Out_fixedTarget_{i}").value == mdl.pc[i] + mdl.jumpOffset[i]
-            #                                        or getattr(dut, f"io_out_stage2Out_fixedTarget_{i}").value == mdl.pc[i] + mdl.jumpOffset[i] - 2**50
-            #                                        or getattr(dut, f"io_out_stage2Out_fixedTarget_{i}").value == mdl.pc[i] + 2
-            #                                        or getattr(dut, f"io_out_stage2Out_fixedTarget_{i}").value == mdl.pc[i] + 4,
-            #f"JAL_TARGET_{i}_CORRECT": lambda dut: getattr(dut, f"io_out_stage2Out_jalTarget_{i}").value == mdl.pc[i] + mdl.jumpOffset[i]
-            #                                        or getattr(dut, f"io_out_stage2Out_jalTarget_{i}").value == mdl.pc[i] + mdl.jumpOffset[i] - 2**50,
-            #f"FIXED_TARGET_{i}_BOUNDARY": lambda dut: getattr(dut, f"io_out_stage2Out_fixedTarget_{i}").value > 0x3_FFFF_FFFF_FFC0,
-            #f"JAL_TARGET_{i}_BOUNDARY": lambda dut: getattr(dut, f"io_out_stage2Out_jalTarget_{i}").value > 0x3_FFFF_FFFF_FFC0
+            f"RAND_PC_VAL_{j}": lambda dut, j=j: getattr(dut, f"io_in_pc_{j}").value > 2**50 - 2**6 - 4
         },
         name=f"TARGET_{i}_COV")
     
