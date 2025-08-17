664bb2e3c9807cc9b8d4077a645c4e7e4c487f17
RubMaker <12312030@mail.sustech.edu.cn>
2025-08-17 16:37:44 +0800

    add test ftb_entry_mem

--- ut_frontend/ftq/ftb_entry_mem/test/ftb_entry_mem_fixture.py
+++ ut_frontend/ftq/ftb_entry_mem/test/ftb_entry_mem_fixture.py
@@ -1,22 +1,23 @@
 import asyncio,toffee,toffee_test
 from toffee import start_clock
-from dut.FtqRedirectMem import DUTFtqRedirectMem
-from ..env import FtqRedirectMemEnv, create_coverage_groups
+from dut.FtbEntryMem import DUTFtbEntryMem
+from ..env import FtbEntryMemEnv, create_coverage_groups
 
 @toffee_test.fixture
-async def ftq_redirect_mem_env(toffee_request: toffee_test.ToffeeRequest):
+async def ftb_entry_mem_env(toffee_request: toffee_test.ToffeeRequest):
     toffee.setup_logging(toffee.INFO)
-    dut = toffee_request.create_dut(DUTFtqRedirectMem)
+    dut = toffee_request.create_dut(DUTFtbEntryMem)
     start_clock(dut)
-    ftq_redirect_mem_env = FtqRedirectMemEnv(dut)
-    ftq_redirect_mem_env.dut.reset.value = 1
-    ftq_redirect_mem_env.dut.Step(10)
-    ftq_redirect_mem_env.dut.reset.value = 0
-    ftq_redirect_mem_env.dut.Step(10)
+    ftb_entry_mem_env = FtbEntryMemEnv(dut)
+    ftb_entry_mem_env.dut.reset.value = 1
+    ftb_entry_mem_env.dut.Step(10)
+    ftb_entry_mem_env.dut.reset.value = 0
+    ftb_entry_mem_env.dut.Step(10)
+    # print(f"all signals: {ftb_entry_mem_env.dut.GetInternalSignalList(use_vpi=False)}")
     dut.InitClock("clock")
     
     print("--- [FIXTURE SETUP] Defining all functional coverage groups... ---")
-    coverage_groups = create_coverage_groups(ftq_redirect_mem_env.bundle, dut)
+    coverage_groups = create_coverage_groups(ftb_entry_mem_env.bundle, dut)
     
     # Add all coverage groups to the test request
     for g in coverage_groups:
@@ -24,7 +25,7 @@ async def ftq_redirect_mem_env(toffee_request: toffee_test.ToffeeRequest):
         dut.StepRis(lambda x: g.sample())
         print(f"Added coverage group: {g.name}")
         
-    yield ftq_redirect_mem_env
+    yield ftb_entry_mem_env
     
     cur_loop = asyncio.get_event_loop()
     for task in asyncio.all_tasks(cur_loop):
