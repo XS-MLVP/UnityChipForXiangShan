7724f30bf417a107a9ddea08a68952705a76d484
CharlesJunic <bidingjun21@mails.ucas.ac.cn>
2025-09-15 13:49:29 +0800

    update itlb env

--- ut_frontend/itlb/itlb_top/toffee_version/agent/itlb_agent.py
+++ ut_frontend/itlb/itlb_top/toffee_version/agent/itlb_agent.py
@@ -16,7 +16,16 @@ from toffee.agent import *
 
 class Requestor0_Agent(Agent):
     @driver_method()
-    async def input(self, req_valid, req_vaddr):
+    async def input(self, req_valid, req_vaddr, n_cycle=1):
         self.dut.requestor[0].req.valid.value = req_valid
         self.dut.requestor[0].req.vaddr.value = req_vaddr
-        return
+        await self.dut.step(n_cycle)
+        resp_paddr = self.dut.requestor[0].resp.paddr.value
+        resp_gpaddr = self.dut.requestor[0].resp.gpaddr.value
+        resp_pbmt = self.dut.requestor[0].resp.pbmt.value
+        resp_miss = self.dut.requestor[0].resp.miss.value
+        resp_isForVSnonLeafPTE = self.dut.requestor[0].resp.isForVSnonLeafPTE.value
+        resp_gpf = self.dut.requestor[0].resp.gpf.value
+        resp_pf = self.dut.requestor[0].resp.pf.value
+        resp_af = self.dut.requestor[0].resp.af.value
+        return (resp_paddr, resp_gpaddr, resp_pbmt, resp_miss, resp_isForVSnonLeafPTE, resp_gpf, resp_pf, resp_af)
--- ut_frontend/itlb/itlb_top/toffee_version/bundle/itlb_bundle.py
+++ ut_frontend/itlb/itlb_top/toffee_version/bundle/itlb_bundle.py
@@ -16,61 +16,61 @@ from toffee.bundle import *
 
 # Sfence Bundle
 class SfenceSignalBundle(Bundle):
-    '''
+    """
     Signals:
 
     rs1, rs2, addr, id, flushpipe, hv, hg
-    '''
+    """
     rs1, rs2, addr, id, flushpipe, hv, hg = Signals(7)
 
 class SfenceBundle(Bundle):
-    '''
+    """
     Signals:
 
     valid, bits.{rs1, rs2, addr, id, flushpipe, hv, hg}
-    '''
+    """
     valid = Signal()
     bits = SfenceSignalBundle.from_prefix('bits_')
 
 # CSR Bundle
 class CSRSatpBundle(Bundle):
-    '''
+    """
     Signals:
 
     mode, asid, ppn, changed
-    '''
+    """
     mode, asid, ppn, changed = Signals(4)
 class CSRVsatpBundle(Bundle):
-    '''
+    """
     Signals:
 
     mode, asid, ppn, changed
-    '''
+    """
     mode, asid, ppn, changed = Signals(4)
 class CSRHgatpBundle(Bundle):
-    '''
+    """
     Signals:
 
     mode, vmid, ppn, changed
-    '''
+    """
     mode, vmid, ppn, changed = Signals(4)
 class CSRPrivBundle(Bundle):
-    '''
+    """
     Signals:
 
     virt, imode
-    '''
+    """
     virt, imode = Signals(2)
 
 class CSRBundle(Bundle):
-    '''
+    """
     Signals:
 
     satp.{mode, asid, ppn, changed},
     vsatp.{mode, asid, ppn, changed},
     hgatp.{mode, vmid, ppn, changed},
     priv.{virt, imode}
-    '''
+    """
     satp = CSRSatpBundle.from_prefix('satp_')
     vsatp = CSRVsatpBundle.from_prefix('vsatp_')
     hgatp = CSRHgatpBundle.from_prefix('hgatp_')
@@ -78,28 +78,28 @@ class CSRBundle(Bundle):
 
 # Requestors Bundle
 class RequestorReqBundle(Bundle):
-    '''
+    """
     Signals:
 
     valid, ready, vaddr
-    '''
+    """
     valid, ready, vaddr = Signals(3)
 
 class RequestorRespBundle(Bundle):
-    '''
+    """
     Signals:
 
     valid, ready, paddr, gpaddr, pbmt, miss, isForVSnonLeafPTE, gpf, pf, af
-    '''
+    """
     valid, ready, paddr, gpaddr, pbmt, miss, isForVSnonLeafPTE, gpf, pf, af = Signals(10)
 
 class RequestorSubBundle(Bundle):
-    '''
+    """
     Signals:
 
     req.{valid, ready, vaddr},
     resp.{valid, ready, paddr, gpaddr, pbmt, miss, isForVSnonLeafPTE, gpf, pf, af}
-    '''
+    """
     req = RequestorReqBundle.from_dict({
         'valid': 'valid',
         'ready': 'ready',
@@ -120,33 +120,33 @@ class RequestorSubBundle(Bundle):
 
 # PTW Bundle
 class PTWReqSubBundle(Bundle):
-    '''
+    """
     Signals:
 
     valid, vpn, s2xlate, getgpa
-    '''
+    """
     valid, vpn, s2xlate, getgpa = Signals(4)
 
 class PTWRespEntryPermBundle(Bundle):
-    '''
+    """
     Signals:
 
     d, a, g, u, x, w, r
-    '''
+    """
     d, a, g, u, x, w, r = Signals(7)
 
 class PTWRespS1EntryBundle(Bundle):
-    '''
+    """
     Signals:
 
     tag, asid, vmid, n, pbmt, level, v, ppn,
     perm.{d, a, g, u, x, w, r}
-    '''
+    """
     tag, asid, vmid, n, pbmt, level, v, ppn = Signals(8)
     perm = PTWRespEntryPermBundle.from_prefix('perm_')
 
 class PTWRespS1Bundle(Bundle):
-    '''
+    """
     Signals:
 
     addr_low, pf, af,
@@ -157,7 +157,7 @@ class PTWRespS1Bundle(Bundle):
     ppn_low[0, 1, 2, 3, 4, 5, 6, 7],
     valididx[0, 1, 2, 3, 4, 5, 6, 7],
     pteidx[0, 1, 2, 3, 4, 5, 6, 7]
-    '''
+    """
     addr_low, pf, af = Signals(3)
     entry = PTWRespS1EntryBundle.from_prefix('entry_')
     ppn_low = SignalList("ppn_low_#", 8)
@@ -165,17 +165,17 @@ class PTWRespS1Bundle(Bundle):
     pteidx = SignalList("pteidx_#", 8)
 
 class PTWRespS2EntryBundle(Bundle):
-    '''
+    """
     Signals:
 
     tag, vmid, n, pbmt, ppn, level,
     perm.{d, a, g, u, x, w, r}
-    '''
+    """
     tag, vmid, n, pbmt, ppn, level = Signals(6)
     perm = PTWRespEntryPermBundle.from_prefix('perm_')
 
 class PTWRespS2Bundle(Bundle):
-    '''
+    """
     Signals:
 
     gpf, gaf,
@@ -183,12 +183,12 @@ class PTWRespS2Bundle(Bundle):
         tag, vmid, n, pbmt, ppn, level,
         perm.{d, a, g, u, x, w, r}
     }
-    '''
+    """
     gpf, gaf = Signals(2)
     entry = PTWRespS2EntryBundle.from_prefix('entry_')
 
 class PTWRespBundle(Bundle):
-    '''
+    """
     Signals:
 
     valid, s2xlate, getgpa,
@@ -209,18 +209,18 @@ class PTWRespBundle(Bundle):
             perm.{d, a, g, u, x, w, r}
         }
     }
-    '''
+    """
     valid, s2xlate, getgpa = Signals(3)
     s1 = PTWRespS1Bundle.from_prefix('bits_s1_')
     s2 = PTWRespS2Bundle.from_prefix('bits_s2_')
 
 class PTWBundle(Bundle):
-    '''
+    """
     Signals:
 
     req.{
         valid, vpn, s2xlate, getgpa
-    }
+    },
     resp.{
         valid, s2xlate, getgpa,
         s1.{
@@ -241,12 +241,12 @@ class PTWBundle(Bundle):
             }
         }
     }
-    '''
+    """
     req = BundleList(PTWReqSubBundle, "req_#_", 3)
     resp = PTWRespBundle.from_prefix('resp_')
 
 class TLBBundle(Bundle):
-    '''
+    """
     Signals:
 
     hartid,
@@ -289,7 +289,7 @@ class TLBBundle(Bundle):
             }
         }
     }
-    '''
+    """
     hartid = Signal()
     sfence = SfenceBundle.from_prefix('sfence_')
     csr = CSRBundle.from_prefix('csr_')
